945447124db84d65cae1c51cf668e02a0a409791
Brian Pontarelli <brian@fusionauth.io>
2022-11-27 16:33:07 -0700

    Adding Tomcat setup for load testing

---
+++ load-tests/tomcat/src/main/java/io/fusionauth/http/load/LoadServlet.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package io.fusionauth.http.load;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Base64;
+
+public class LoadServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    try (InputStream is = req.getInputStream()) {
+      byte[] body = is.readAllBytes();
+      String result = Base64.getEncoder().encodeToString(body);
+      res.setStatus(200);
+
+      PrintWriter writer = res.getWriter();
+      writer.write(result);
+      writer.flush();
+      writer.close();
+    } catch (Exception e) {
+      res.setStatus(500);
+    }
+  }
+}
