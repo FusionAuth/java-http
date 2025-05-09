ad429707aea25eea0a59b6508f8ccfb566fb9684
Daniel DeGroff <daniel@fusionauth.io>
2025-05-09 13:13:04 -0600

    add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.fusionauth.http {
+  exports io.fusionauth.http.client;
+  exports io.fusionauth.http.io;
+  exports io.fusionauth.http.log;
+  exports io.fusionauth.http.security;
+  exports io.fusionauth.http.server;
+  exports io.fusionauth.http.util;
+}
