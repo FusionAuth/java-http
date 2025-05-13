5a2c0594f93de5f819b2088d294022a80ebb7d5f
Daniel DeGroff <daniel@fusionauth.io>
2025-05-13 17:32:06 -0600

    working

--- src/main/java/io/fusionauth/http/server/internal/HTTPServerThread.java
+++ src/main/java/io/fusionauth/http/server/internal/HTTPServerThread.java
@@ -184,7 +184,7 @@ public class HTTPServerThread extends Thread {
           long readThroughput = -1;
           long writeThroughput = -1;
 
-          String badClientReason = "[" + threadId + "] Checking worker in state [" + state + "]";
+          String badClientReason = "[" + threadId + "] Check worker in state [" + state + "]";
           if (state == HTTPWorker.State.Read) {
             // Here the SO_TIMEOUT set above or the Keep-Alive timeout in HTTPWorker will dictate if the socket has timed out. This prevents slow readers
             // or network issues where the client reads 1 byte per timeout value (i.e. 1 byte per 2 seconds or something like that)
